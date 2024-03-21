5ad59e4bb79b88ac3fc66232856c25530d5958df
Rami Hammoud <71185989+Hammoud01881385@users.noreply.github.com>
2024-03-21 16:33:48 -0400

    Movie home/details UI updates

--- app/src/main/java/com/example/tracksnap/MovieDetailsFragment.java
+++ app/src/main/java/com/example/tracksnap/MovieDetailsFragment.java
@@ -4,61 +4,69 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MovieDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
 public class MovieDetailsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MovieDetailsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MovieDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MovieDetailsFragment newInstance(String param1, String param2) {
-        MovieDetailsFragment fragment = new MovieDetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private TextView titleTextView, overviewTextView, releaseDateTextView, voteAverageTextView, genreTextView;
+    private ImageView imageView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_movie_details, container, false);
+
+        // Initialize views
+        titleTextView = view.findViewById(R.id.title_txt2);
+        overviewTextView = view.findViewById(R.id.overview_txt2);
+        releaseDateTextView = view.findViewById(R.id.release_txt2);
+        voteAverageTextView = view.findViewById(R.id.vote_txt2);
+        genreTextView = view.findViewById(R.id.genre_txt2);
+        imageView = view.findViewById(R.id.imageView2);
+
+        // Retrieve movie information from arguments
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            String movieTitle = bundle.getString("title");
+            String movieOverview = bundle.getString("overview");
+            String movieReleaseDate = bundle.getString("releaseDate");
+            double movieVoteAverage = bundle.getDouble("voteAverage");
+            String movieImage = bundle.getString("image");
+            List<String> movieGenreList = bundle.getStringArrayList("genreList");
+
+            // Set data to views
+            titleTextView.setText(movieTitle);
+            overviewTextView.setText(movieOverview);
+            releaseDateTextView.setText(movieReleaseDate);
+            voteAverageTextView.setText(String.valueOf(movieVoteAverage));
+
+            // Set genres
+            StringBuilder genres = new StringBuilder();
+            for (String genre : movieGenreList) {
+                genres.append(genre).append(", ");
+            }
+            // Remove the trailing comma and space
+            if (genres.length() > 2) {
+                genres.setLength(genres.length() - 2);
+            }
+            genreTextView.setText(genres.toString());
+
+            // Load image
+            Glide.with(requireContext())
+                    .load(movieImage)
+                    .into(imageView);
+        } else {
+            Log.e("MovieDetailsFragment", "Bundle is null");
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_details, container, false);
+        return view;
     }
 }
